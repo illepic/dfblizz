2e1dc7af29857e913fdef4e6066245293ceaa4a5
Chris Bloom <bloomcb@gmail.com>
2016-11-06 09:34:20 -0800

    Blizzard api params passed to service, simple command for now

--- src/AppBundle/Command/AppGetBlizzDataCommand.php
+++ src/AppBundle/Command/AppGetBlizzDataCommand.php
@@ -7,34 +7,34 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use BlizzardApi\BlizzardClient;
-use BlizzardApi\Service\WorldOfWarcraft;
+
 
 class AppGetBlizzDataCommand extends ContainerAwareCommand
 {
+
     protected function configure()
     {
         $this
             ->setName('app:get-blizz-data')
             ->setDescription('Query the Blizzard API to return to be written to disk')
-            ->addArgument('key', InputArgument::REQUIRED, 'Blizzard API key')
-            ->addArgument('secret', InputArgument::REQUIRED, 'Blizzard API secret')
+//            ->addArgument('key', InputArgument::REQUIRED, 'Blizzard API key')
+//            ->addArgument('secret', InputArgument::REQUIRED, 'Blizzard API secret')
 //            ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
         ;
+
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
-        $client = new BlizzardClient($input->getArgument('key'));
-        $wow = new WorldOfWarcraft($client->setAccessToken($input->getArgument('secret')));
-
-        // Use API method for getting specific data
-        $response = $wow->getCharacter('gorgonnash', 'elapsed', [
-          'fields' => '',
+        $api = $this->getContainer()->get('app.wow_api');
+        $response = $api->wow->getCharacter('gorgonnash', 'elapsed', [
+           'fields' => '',
         ]);
 
+        $output->writeln('Hello');
         $output->writeln($response->getBody()->getContents());
+
     }
 
 }
---
+++ src/AppBundle/Utils/WoWApi.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: illepic
+ * Date: 11/6/16
+ * Time: 7:33 AM
+ */
+
+namespace AppBundle\Utils;
+
+use BlizzardApi\BlizzardClient;
+use BlizzardApi\Service\WorldOfWarcraft;
+
+class WoWApi
+{
+    public $wow;
+
+    /**
+     * WoWApi constructor.
+     * @param String $key
+     * @param String $secret
+     */
+    public function __construct(String $key, String $secret)
+    {
+        $client = new BlizzardClient($key);
+        $this->wow = new WorldOfWarcraft($client->setAccessToken($secret));
+    }
+
+}
